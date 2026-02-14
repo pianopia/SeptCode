1c4d420ec73f0aa41228a9070fb460780bdfe6d1
pianopia <grandpianoaoa@gmail.com>
2026-02-14 20:59:54 +0900

    update: SEO

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
   title: "SeptCode",
   description: "7行コードを共有するSNS",
-  keywords: ["SeptCode", "セプトコード", "Sept", "コードだけ", "プログラミング", "プログラミングSNS", "プログラミングコミュニティ", "プログラマー", "プログラミング学習", "セプト", "7行", "コード共有", "コード共有SNS", "SNS"],
+  keywords: ["SeptCode", "セプトコード", "SNSアプリ", "新しいSNS", "Sept", "コードだけ", "プログラミングだけ", "プログラミングSNS", "プログラミングコミュニティ", "プログラマー", "プログラミング学習", "7行", "コード共有", "コード共有SNS"],
   alternates: {
     canonical: "/"
   },
